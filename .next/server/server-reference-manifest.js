91df0061b929a5567031740396e788fcd5c313ff
kmuralikum <kmuralikum2002@gmail.com>
2024-02-01 20:21:50 +0530

    first commit

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"bHTPWS3/40VgpetnplnHsttzmteylg5npj8sfoixACA=\"}"
---
+++ .next/static/chunks/app/services/page-03aa4860fe712bc4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[469],{3220:function(e,n,s){Promise.resolve().then(s.bind(s,4341)),Promise.resolve().then(s.bind(s,6323)),Promise.resolve().then(s.t.bind(s,5250,23))}},function(e){e.O(0,[285,241,86,323,341,971,938,744],function(){return e(e.s=3220)}),_N_E=e.O()}]);
