b44cf202462ee0ab725e254a86cdc914f3ff3046
kmuralikum <kmuralikum2002@gmail.com>
2024-02-06 20:46:55 +0530

    🌈 style: update Pricing.js

--- components/Pricing.js
+++ components/Pricing.js
@@ -4,7 +4,7 @@ import { Nav, Tab } from "react-bootstrap";
 
 const Pricing = ({ extraClass }) => {
   return (
-    <section className={`pricing-area pt-130 rpt-100 rel z-1 ${extraClass}`}>
+    <section className={`pricing-area pt-60  rpt-100 rel z-1 ${extraClass}`}>
       <div className="container">
         <div className="row justify-content-center">
           <div className="col-xl-12">
