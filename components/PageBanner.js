98c929febe1a3b2ad70bb32c7a553159aa38892d
kmuralikum <kmuralikum2002@gmail.com>
2024-02-07 17:40:39 +0530

    🌈 style: update PageBanner.js

--- components/PageBanner.js
+++ components/PageBanner.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const PageBanner = ({ pageName }) => {
   return (
-    <section className="page-banner-area pt-200 rpt-140 pb-100 rpb-60 rel z-1 text-center">
+    <section className="page-banner-area pt-80 rpt-140 pb-100 rpb-60 rel z-1 text-center">
       <div className="container">
         <div className="banner-inner text-white">
           <h1 className="page-title wow fadeInUp delay-0-2s">{pageName}</h1>
