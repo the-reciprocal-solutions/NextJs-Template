a15ceb1adaa4347c98c9b010feed588aedb7c8b7
kmuralikum <kmuralikum2002@gmail.com>
2024-02-09 18:17:14 +0530

    🦄 refactor: create Gallery.js

---
+++ components/Gallery.js
@@ -0,0 +1,47 @@
+import PageBanner from "@/components/PageBanner";
+import { Fragment } from "react";
+
+import dynamic from "next/dynamic";
+const ProjectGridIsotop = dynamic(
+  () => import("@/components/ProjectGridIsotop"),
+  {
+    ssr: false,
+  }
+);
+
+export const metadata = {
+  title: "Gallery",
+};
+
+
+export const Gallery = () => {
+    return (
+      <Fragment>
+      <PageBanner pageName={"Gallery"} />
+        <section className="projects-area pt-40 pb-130 rpb-100 rel z-1">
+          <div className="container">
+            <ProjectGridIsotop />
+            <div className="project-btn text-center wow fadeInUp delay-0-2s">
+              {/* <Link legacyBehavior href="/projects">
+                <a className="theme-btn">
+                  View More Projects <i className="far fa-angle-right" />
+                </a>
+              </Link> */}
+            </div>
+          </div>
+          <div className="bg-lines">
+            <span />
+            <span />
+            <span />
+            <span />
+            <span />
+            <span />
+            <span />
+            <span />
+            <span />
+            <span />
+          </div>
+        </section>
+        </Fragment>
+    );
+  };
