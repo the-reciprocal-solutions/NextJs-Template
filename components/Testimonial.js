1f171e7bcaf0c47ecb0e250819ae0d437a528375
kmuralikum <kmuralikum2002@gmail.com>
2024-02-15 12:59:37 +0530

    🦄 refactor:Avatar changed

--- components/Testimonial.js
+++ components/Testimonial.js
@@ -73,7 +73,7 @@ export default class Testimonial extends Component {
                   <div className="testimonial-item wow fadeInUp delay-0-4s">
                     <div className="author">
                       <img
-                        src="assets/images/testimonials/Female.png"
+                        src="assets/images/testimonials/Female2.png"
                         alt="Author"
                       />
                     </div>
@@ -88,7 +88,7 @@ export default class Testimonial extends Component {
                   <div className="testimonial-item wow fadeInUp delay-0-2s">
                     <div className="author">
                       <img
-                        src="assets/images/testimonials/Female.png"
+                        src="assets/images/testimonials/Female3.png"
                         alt="Author"
                       />
                     </div>
