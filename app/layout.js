324cda81408bd713b4481a2c1a833b0b09bedf46
kmuralikum <kmuralikum2002@gmail.com>
2024-02-27 19:21:52 +0530

    🦄 refactor: update layout.js

--- app/layout.js
+++ app/layout.js
@@ -33,7 +33,7 @@ export const metadata = {
     template:
       "Essensuals Toni & Guy || %s",
     default:
-      "Essensuals by Toni & Guy nava india || Home", // a default is required when creating a template
+      "Essensuals by Toni & Guy nava india", // a default is required when creating a template
   },
   description: "Essensuals Toni & Guy",
 };
